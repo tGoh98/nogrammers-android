c7691b269f9fe8285713842e0c5c709a2050ae32
tGoh98 <tmg5@rice.edu>
2021-01-01 21:44:29 -0600

    updated view for shoutouts

--- app/src/main/java/com/example/nogrammers_android/ShoutoutsAdapter.kt
+++ app/src/main/java/com/example/nogrammers_android/ShoutoutsAdapter.kt
@@ -3,25 +3,28 @@ package com.example.nogrammers_android
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class ShoutoutsAdapter(private val data: List<Shoutouts>) : RecyclerView.Adapter<ShoutoutsAdapter.ItemViewHolder>() {
+class ShoutoutsAdapter(private val data: List<Shoutouts>) : RecyclerView.Adapter<ShoutoutsAdapter.ShoutoutViewHolder>() {
 
-    class ItemViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
-        val textView: TextView = view.findViewById(R.id.shoutout_title)
-    }
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ShoutoutViewHolder {
         val adapterLayout = LayoutInflater.from(parent.context)
-            .inflate(R.layout.shoutout_item, parent, false)
-        return ItemViewHolder(adapterLayout)
+            .inflate(R.layout.shoutout_item_new, parent, false)
+        return ShoutoutViewHolder(adapterLayout)
     }
 
-    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: ShoutoutViewHolder, position: Int) {
         val item = data[position]
-        holder.textView.text = item.author
+        holder.msg.text = item.author
+        holder.pfp.setImageResource(R.drawable.km1)
     }
 
     override fun getItemCount() = data.size
+
+    class ShoutoutViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val msg: TextView = itemView.findViewById(R.id.msg)
+        val pfp: ImageView = itemView.findViewById(R.id.pfp)
+    }
 }
