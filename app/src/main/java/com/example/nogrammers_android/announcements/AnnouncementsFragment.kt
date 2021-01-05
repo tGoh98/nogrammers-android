32260943247db1d4b0134186d3a6f12ad1704e93
tGoh98 <tmg5@rice.edu>
2021-01-04 19:58:30 -0600

    added fab

--- app/src/main/java/com/example/nogrammers_android/announcements/AnnouncementsFragment.kt
+++ app/src/main/java/com/example/nogrammers_android/announcements/AnnouncementsFragment.kt
@@ -8,6 +8,7 @@ import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.example.nogrammers_android.R
 import com.example.nogrammers_android.databinding.FragmentAnnouncementsBinding
+import com.google.android.material.snackbar.Snackbar
 
 /**
  * Announcements tab
@@ -18,23 +19,30 @@ class AnnouncementsFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
+        /* Inflate the layout for this fragment with data binding */
         val binding = DataBindingUtil.inflate<FragmentAnnouncementsBinding>(
             inflater,
             R.layout.fragment_announcements,
             container,
             false
         )
-
-        val adapter = AnnouncementsAdapter((1..16).map {
+        // TODO: dynamically populate the content for the recycler view
+        val adapter = AnnouncementsAdapter((1..10).map {
             Announcement(
                 "Fire drill $it",
                 "Fire drill wee woo Fire drill wee woo Fire drill wee woo wee woo wee woo wee woo wee woo wee woo wee woo wee woo wee woo wee woo wee woo wee woo wee woo wee woo wee woo wee woo beep beep beep beep beep beep beep beep",
                 "snoopy$it"
             )
         })
-
         binding.announcementsView.adapter = adapter
+
+        /* Listener for create announcement button */
+        val createBtn = binding.createAnnouncementBtn
+        createBtn.setOnClickListener { view ->
+            Snackbar.make(view, "TODO: create a new announcement", Snackbar.LENGTH_LONG)
+                .setAction("Action", null)
+                .show()
+        }
 
         return binding.root
     }
