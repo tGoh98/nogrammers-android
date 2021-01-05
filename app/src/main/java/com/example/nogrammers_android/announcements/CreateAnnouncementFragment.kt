701fb7c830a6f64f4746c5614f39ec4fb9186027
tGoh98 <tmg5@rice.edu>
2021-01-05 15:25:05 -0600

    Styled create announcements and added skeleton functionality

--- app/src/main/java/com/example/nogrammers_android/announcements/CreateAnnouncementFragment.kt
+++ app/src/main/java/com/example/nogrammers_android/announcements/CreateAnnouncementFragment.kt
@@ -1,17 +1,26 @@
 package com.example.nogrammers_android.announcements
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import com.example.nogrammers_android.R
+import com.example.nogrammers_android.databinding.FragmentCreateAnnouncementBinding
+import com.google.android.material.snackbar.Snackbar
 
 /**
  * Fragment for creating a new announcement
  */
 class CreateAnnouncementFragment : Fragment() {
+
+    /* Shared view model */
+    private val model: AnnouncementsViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -19,21 +28,49 @@ class CreateAnnouncementFragment : Fragment() {
     ): View? {
 
         /* Inflate the layout for this fragment */
-        val view = inflater.inflate(R.layout.fragment_create_announcement, container, false)
+        val binding = DataBindingUtil.inflate<FragmentCreateAnnouncementBinding>(
+            inflater,
+            R.layout.fragment_create_announcement,
+            container,
+            false
+        )
 
         /* Add listener for cancel button */
-        val cancelBtn = view.findViewById<Button>(R.id.cancelAnnounceBtn)
-        cancelBtn.setOnClickListener { hideNewAnnouncementFrag() }
+        binding.cancelAnnounceBtn.setOnClickListener {
+            closeTab(binding.newAnnouncementTitle, binding.newAnnouncementTxt)
+        }
 
-        return view
+        /* Listener for post announcements */
+        binding.postNewAnnouncementBtn.setOnClickListener {
+            val anTitle = binding.newAnnouncementTitle.text.toString()
+            val anMsg = binding.newAnnouncementTxt.text.toString()
+            val pinToTop = binding.pinAnnouncment.isChecked
+            val postToFb = binding.postToFb.isChecked
+            val sendToListserv = binding.sendToListserv.isChecked
+
+            Snackbar.make(binding.root, "$anTitle\n$anMsg", Snackbar.LENGTH_LONG)
+                .setAction("Action", null)
+                .show()
+//            Snackbar.make(binding.root, "pinToTop: $pinToTop postToFb: $postToFb sendToListserv: $sendToListserv", Snackbar.LENGTH_LONG)
+//                .setAction("Action", null)
+//                .show()
+            // TODO: update database
+
+            closeTab(binding.newAnnouncementTitle, binding.newAnnouncementTxt)
+        }
+
+        return binding.root
     }
 
-    /**
-     * Hides this fragment. Perhaps find some way to move this logic to AnnouncementsFragment.kt?
-     */
-    private fun hideNewAnnouncementFrag() {
-        val sfm = activity?.supportFragmentManager ?: return
-        val fragment = sfm.findFragmentByTag("createAnnouncement")
-        if (fragment != null) sfm.beginTransaction().remove(fragment).commit()
+    private fun closeTab(anTitle: EditText, anTxt: EditText) {
+        /* Update create mode */
+        model.createMode.value = false
+
+        /* Hide keyboard */
+        val imm = activity?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(requireView().windowToken, 0)
+        anTitle.text.clear()
+        anTxt.text.clear()
     }
+
 }
