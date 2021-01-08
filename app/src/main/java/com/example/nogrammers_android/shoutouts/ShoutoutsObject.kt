54b58dee9de890bae3cd9885484a3423ced1fba6
Johnny Ho <jdh16@rice.edu>
2021-01-08 15:40:27 -0500

    Changed ShoutoutsFragment to use deserialization instead

---
+++ app/src/main/java/com/example/nogrammers_android/shoutouts/ShoutoutsObject.kt
@@ -0,0 +1,10 @@
+package com.example.nogrammers_android.shoutouts
+
+// To deserialize data from the database
+class ShoutoutsObject {
+    // TODO should these be values instead?
+    // These are just default values, will be overwritten when constructor is called
+    var author: String = "author"
+    var date: String = "0"
+    var msg: String = "test"
+}
