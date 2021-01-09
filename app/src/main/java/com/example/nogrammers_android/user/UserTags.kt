737310c485d6692b3f9bf3a81391961f08719ae6
tGoh98 <tmg5@rice.edu>
2021-01-09 13:08:49 -0600

    Can read and write user objects to/from db

---
+++ app/src/main/java/com/example/nogrammers_android/user/UserTags.kt
@@ -0,0 +1,82 @@
+package com.example.nogrammers_android.user
+
+enum class UserTags {
+    DiversityFacilitator {
+        override fun toString() = "Diversity Facilitator"
+    },
+    PAA {
+        override fun toString() = "PAA"
+    },
+    PHA {
+        override fun toString() = "PHA"
+    },
+    RHA {
+        override fun toString() = "RHA"
+    },
+    StriveLiason {
+        override fun toString() = "Strive Liason"
+    },
+    CCCRep {
+        override fun toString() = "CCC Rep"
+    },
+    CJ {
+        override fun toString() = "CJ"
+    },
+    FreshRep {
+        override fun toString() = "Freshman Rep"
+    },
+    SophRep {
+        override fun toString() = "Sophomore Rep"
+    },
+    JuniorRep {
+        override fun toString() = "Junior Rep"
+    },
+    SeniorRep {
+        override fun toString() = "Senior Rep"
+    }
+}
+
+/*
+LVP
+Spirit Committee
+Spirit Head
+Socials Committee
+Socials Head
+Culture and Art Committee
+Culture and Arts Head
+Improvements Committee
+Improvements Head
+Associates Committee
+Associates Head
+Senior Head
+Beer Bike Committee
+Beer Bike Head
+Beer Bike Captain
+Merch Rep
+Eco Rep
+SMR
+Sports Rep
+Kitchen Rep
+Tech Rep
+Historians
+Service Rep
+TOKC Rep
+Alumni Rep
+RPC Rep
+International Student Rep
+OC Rep
+H&D Rep
+Laundry Rep
+New Student Committee
+SA Senator
+SA NSR
+VP
+President
+Treasurer
+U Court Rep
+Honor Council Rep
+OWeek Coordinator
+OWeek Advisor
+PCA
+
+ */
