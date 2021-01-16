3ee7a5835603d5d6af2ffa49513f3271537607ac
Johnny Ho <jdh16@rice.edu>
2021-01-15 19:24:06 -0500

    Temporary commit to save changes, working on updated shoutouts

--- app/src/main/java/com/example/nogrammers_android/shoutouts/BindingUtils.kt
+++ app/src/main/java/com/example/nogrammers_android/shoutouts/BindingUtils.kt
@@ -34,3 +34,33 @@ fun ImageView.setPfp(item: Shoutout) {
     if (item.pfp == "") setImageResource(R.drawable.km1)
     else TODO("need to implement custom images")
 }
+
+@BindingAdapter("author2")
+fun TextView.setAuthor(item: Shoutout2) {
+    text = item.author
+}
+
+@BindingAdapter("msg2")
+fun TextView.setMsg(item: Shoutout2) {
+    text = item.msg
+}
+
+@BindingAdapter("date2")
+fun TextView.setDate(item: Shoutout2) {
+    val sdf = SimpleDateFormat("MM/dd/yy hh:mm a", Locale.getDefault())
+    val date = java.util.Date(item.date.toLong())
+    text = sdf.format(date)
+//    text = java.util.Date(item.date.toLong()).toString()
+
+}
+
+@BindingAdapter("pfp2")
+fun ImageView.setPfp(item: Shoutout2) {
+    if (item.pfp == "") setImageResource(R.drawable.km1)
+    else TODO("need to implement custom images")
+}
+
+@BindingAdapter("rooCount")
+fun TextView.setRooCount(item: Shoutout2) {
+    rooCount = item.rooCount
+}
